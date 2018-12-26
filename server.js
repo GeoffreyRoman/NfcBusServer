0c960b1f23c0b7a8727906180a3c0b6ff1c59848
Roman Geoffrey <geoffrey.roman@etu.unice.fr>
2018-12-26 13:00:08 +0100

    Suppression ligne inutile

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-//L'application requiert l'utilisation du module Express.
-//La variable express nous permettra d'utiliser les fonctionnalités du module Express.
 var express = require("express");
-// Nous définissons ici les paramètres du serveur.
 var mysql = require("mysql");
 
 var con = mysql.createConnection({
@@ -12,14 +9,6 @@ var con = mysql.createConnection({
   database: "nfcbus",
   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock" //for mac and linux
 });
-var fs = require("fs"),
-  readline = require("readline");
-
-// var rd = readline.createInterface({
-//   input: fs.createReadStream("stopTimes.sql"),
-//   console: false
-// });
-console.log(con);
 
 con.connect(function(err) {
   var i = 0;
@@ -50,11 +39,8 @@ con.connect(function(err) {
 var hostname = "localhost";
 var port = 3000;
 
-// Nous créons un objet de type Express.
 var app = express();
 
-//Afin de faciliter le routage (les URL que nous souhaitons prendre en charge dans notre API), nous créons un objet Router.
-//C'est à partir de cet objet myRouter, que nous allons implémenter les méthodes.
 var myRouter = express.Router();
 
 myRouter
@@ -124,7 +110,6 @@ myRouter
     });
   });
 
-// Nous demandons à l'application d'utiliser notre routeur
 app.use(myRouter);
 
 // Démarrer le serveur
